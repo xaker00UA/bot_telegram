4d787877458e1273a812e8b1a1f7d588b24535a4
Ivan <ivanbozhkod@gmail.com>
2025-06-05 22:26:14 +0300

    Hotfix

--- src/handlers/basic.py
+++ src/handlers/basic.py
@@ -3,7 +3,6 @@ from aiogram.filters import CommandStart
 
 
 from src.config.settings import settings
-from src.keyboard.main import reply_kb
 
 basic_router = Router()
 
@@ -11,4 +10,4 @@ basic_router = Router()
 @basic_router.message(CommandStart())
 async def start_handler(message: types.Message, locale):
     data = settings.get_localized_text(locale)
-    await message.answer(data.get("welcome"), reply_markup=reply_kb)
+    await message.answer(data.get("welcome"))
--- src/keyboard/main.py
+++ src/keyboard/main.py
@@ -1,12 +1,7 @@
 from aiogram.types import (
-    ReplyKeyboardMarkup,
-    KeyboardButton,
     InlineKeyboardMarkup,
     InlineKeyboardButton,
 )
-
-kb = [[KeyboardButton(text="/insurance_start")]]
-reply_kb = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
 
 def get_confirm_keyboard(yes_callback: str, no_callback: str) -> InlineKeyboardMarkup:
